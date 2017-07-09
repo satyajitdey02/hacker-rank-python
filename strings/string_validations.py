639a770753739f4193e56e04c55c75a2a6c7a6c6
satyajit <satyajit02@gmail.com>
2017-07-09 13:48:17 +0600

    hrp: V2-Solve String validation problem

--- strings/string_validations.py
+++ strings/string_validations.py
@@ -2,34 +2,23 @@ if __name__ == '__main__':
     s = input()
 
     isalnum = False
-    for c in s:
-        if c.isalnum():
-            isalnum = True
-            break
-
     isalpha = False
-    for c in s:
-        if c.isalpha():
-            isalpha = True
-            break
-
     isdigit = False
-    for c in s:
-        if c.isdigit():
-            isdigit = True
-            break
-
     islower = False
+    isupper = False
+
     for c in s:
         if c.islower():
+            isalnum = True
+            isalpha = True
             islower = True
-            break
-
-    isupper = False
-    for c in s:
-        if c.isupper():
+        elif c.isupper():
+            isalnum = True
+            isalpha = True
             isupper = True
-            break
+        elif c.isdigit():
+            isdigit = True
+            isalnum = True
 
     print(isalnum)
     print(isalpha)
